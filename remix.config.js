502f2b1a042755b4e25eae160c342add74170bb7
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-06-15 05:45:25 +0700

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  future: {
-    unstable_tailwind: true,
-  },
-
+  appDirectory: "app",
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  assetsBuildDirectory: "public/build",
 };
